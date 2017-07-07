56b8a48391846f867413701c48941ee371100bcb
Evan Hahn <me@evanhahn.com>
2017-07-06 18:54:53 -0700

    Add Expect-CT code and tests

--- src/ring_secure_headers/core.clj
+++ src/ring_secure_headers/core.clj
@@ -1,8 +1,31 @@
-(ns ring-secure-headers.core)
+(ns ring-secure-headers.core
+  (:require [clojure.string :refer [join]]))
 
 (defn expect-ct
   ([handler] (expect-ct handler {}))
 
   ([handler options]
-    (fn [request]
-      (handler (assoc-in request [:headers "expect-ct"] "max-age=0")))))
+   (when-not (map? options)
+     (throw (ex-info "options must be a map" {:options options})))
+
+   (let [with-enforce (if (:enforce? options) ["enforce"] [])
+
+         raw-max-age (get options :max-age 0)
+         max-age (if (and (integer? raw-max-age) (>= raw-max-age 0))
+                   raw-max-age
+                   (throw (ex-info "max-age must be 0 or greater" {:max-age raw-max-age})))
+         with-max-age (conj with-enforce (str "max-age=" max-age))
+
+         report-uri (:report-uri options)
+         with-report-uri (cond
+                           (string? report-uri)
+                           (conj with-max-age (str "report-uri=\"" report-uri "\""))
+                           (contains? options :report-uri)
+                           (throw (ex-info "report-uri must be a string" {:report-uri report-uri}))
+                           :default
+                           with-max-age)
+
+         result (join "; " with-report-uri)]
+
+     (fn [request]
+       (handler (assoc-in request [:headers "expect-ct"] result))))))
--- test/ring_secure_headers/expect_ct_test.clj
+++ test/ring_secure_headers/expect_ct_test.clj
@@ -2,12 +2,52 @@
   (:require [clojure.test :refer :all]
             [ring-secure-headers.core :refer [expect-ct]]))
 
+(defn- dummy-handler [_])
+
 (defn- expect-handler [expected]
   (fn [request]
     (let [actual (get-in request [:headers "expect-ct"])]
       (is (= expected actual)))))
 
-(deftest expect-ct-test
+(defn- test-helper [options expected]
+  (let [handler (expect-ct (expect-handler expected) options)]
+    (handler {})))
+
+(deftest expect-ct-happy-path-test
   (testing "sets max-age to 0 when given no options"
     (let [handler (expect-ct (expect-handler "max-age=0"))]
-      (handler {}))))
+      (handler {})))
+  (testing "sets max-age to 0 when given an empty map"
+    (test-helper {} "max-age=0"))
+  (testing "sets max-age when provided as an integer"
+    (test-helper {:max-age 0} "max-age=0")
+    (test-helper {:max-age 123} "max-age=123"))
+  (testing "enforcement"
+    (test-helper {:enforce? true} "enforce; max-age=0"))
+  (testing "explicitly disabling enforcement"
+    (test-helper {:enforce? false} "max-age=0"))
+  (testing "sets report-uri"
+    (test-helper {:report-uri "https://example.com/report"}
+                 "max-age=0; report-uri=\"https://example.com/report\""))
+  (testing "sets enforcement, max-age, and report-uri all together"
+    (test-helper {:max-age 123
+                  :enforce? true
+                  :report-uri "https://example.com/r"}
+                 "enforce; max-age=123; report-uri=\"https://example.com/r\"")))
+
+(deftest expect-ct-sad-path-test
+  (testing "throws when not passed a map"
+    (is (thrown? Exception (expect-ct dummy-handler 123)))
+    (is (thrown? Exception (expect-ct dummy-handler [:max-age 123]))))
+  (testing "throws when passed a non-integer max-age"
+    (is (thrown? Exception (expect-ct dummy-handler {:max-age nil})))
+    (is (thrown? Exception (expect-ct dummy-handler {:max-age 123.456})))
+    (is (thrown? Exception (expect-ct dummy-handler {:max-age "123"}))))
+  (testing "throws when passed a negative max-age"
+    (is (thrown? Exception (expect-ct dummy-handler {:max-age -0.1})))
+    (is (thrown? Exception (expect-ct dummy-handler {:max-age -1})))
+    (is (thrown? Exception (expect-ct dummy-handler {:max-age -1.5})))
+    (is (thrown? Exception (expect-ct dummy-handler {:max-age -123}))))
+  (testing "throws when passed a non-string report-uri"
+    (is (thrown? Exception (expect-ct dummy-handler {:report-uri nil})))
+    (is (thrown? Exception (expect-ct dummy-handler {:report-uri 123})))))
