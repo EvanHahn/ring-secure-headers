a9fbf297484a88b4119e195383720a765b8b90d1
Evan Hahn <me@evanhahn.com>
2017-07-06 17:28:08 -0700

    Initial commit

---
+++ src/ring_secure_headers/core.clj
@@ -0,0 +1,6 @@
+(ns ring-secure-headers.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/ring_secure_headers/core_test.clj
@@ -0,0 +1,7 @@
+(ns ring-secure-headers.core-test
+  (:require [clojure.test :refer :all]
+            [ring-secure-headers.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
