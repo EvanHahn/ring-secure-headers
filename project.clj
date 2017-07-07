16d894c740f338ead1baf23459c2f1b983080b46
Evan Hahn <me@evanhahn.com>
2017-07-06 17:30:59 -0700

    Update metadata in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject ring-secure-headers "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Ring HTTP headers for security"
+  :url "https://github.com/EvanHahn/ring-secure-headers"
+  :license {:name "Unlicense"
+            :url "http://unlicense.org"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
